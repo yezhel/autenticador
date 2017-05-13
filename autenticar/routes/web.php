ec5d3d609cadb25dbd33982493b7eb61b43a17bc
yezhel <yezhelmr@gmail.com>
2017-05-13 00:04:27 -0500

    proceso
    proceso

---
+++ autenticar/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['middleware' => 'web'], function(){
+	Route::get('/', function () {
+	    return view('welcome');
+	});
+	Route::auth();
+
+	Route::get('/home', 'HomeController@index');
+
+	Route::get('admins/login', 'AdministratorsController@showLoginForm');
+
+	Route::post('admins/login', 'AdministratorsController@login');
+
+	Route::post('admins/area', 'AdministratorsController@secret');
+	
+});
